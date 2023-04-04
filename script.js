2112b852b48c02dd9f1f220bc43f46abad6a1592
Orel Tamari <44579462+oreltamari@users.noreply.github.com>
2023-04-04 23:13:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(window).scroll(function () {
   $(".sticky-content ").css({
     opacity: function () {
       var elementHeight = $(this).height(),
-        opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 0.15;
+        opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 0.3;
       return opacity;
     },
   });
