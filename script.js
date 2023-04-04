caee8db0e25ef5bcd06a223d49b695c5327d3962
Orel Tamari <44579462+oreltamari@users.noreply.github.com>
2023-04-04 23:22:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 if (
   (window.innerWidth ||
     document.documentElement.clientWidth ||
-    document.body.clientWidth) < 767
+    document.body.clientWidth) < 600
 ) {
   $(window).scroll(function () {
     var scrollTop = $(this).scrollTop();
     $(".sticky-content ").css({
       opacity: function () {
         var elementHeight = $(this).height(),
-          opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 0.3;
+          opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 2;
         return opacity;
       },
     });
