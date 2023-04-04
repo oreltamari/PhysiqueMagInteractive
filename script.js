aaf3e8f6badde287350fa8db83da66ab0c989ef9
Orel Tamari <44579462+oreltamari@users.noreply.github.com>
2023-04-04 23:22:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-    if ((window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth) < 767) {
-       $(window).scroll(function () {
-  var scrollTop = $(this).scrollTop();
-  $(".sticky-content ").css({
-    opacity: function () {
-      var elementHeight = $(this).height(),
-        opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 5;
-      return opacity;
-    },
+if (
+  (window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth) < 767
+) {
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    $(".sticky-content ").css({
+      opacity: function () {
+        var elementHeight = $(this).height(),
+          opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 0.3;
+        return opacity;
+      },
+    });
   });
-}); else {
-    }
-$(window).scroll(function () {
-  var scrollTop = $(this).scrollTop();
-  $(".sticky-content ").css({
-    opacity: function () {
-      var elementHeight = $(this).height(),
-        opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 0.3;
-      return opacity;
-    },
+} else {
+  $(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    $(".sticky-content ").css({
+      opacity: function () {
+        var elementHeight = $(this).height(),
+          opacity = (1 - (elementHeight - scrollTop) / elementHeight) * 0.3;
+        return opacity;
+      },
+    });
   });
-});
-    }
+}
